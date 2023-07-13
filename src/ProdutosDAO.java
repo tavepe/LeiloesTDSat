1f3209254b3d4287de12f0f18134dcbce6fe7679
Gustavo Zanin <taveppe23@gmail.com>
2023-07-13 01:10:43 -0300

    Botão de salvar cadastrando produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,7 +12,10 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class ProdutosDAO {
@@ -24,8 +27,21 @@ public class ProdutosDAO {
     
     public void cadastrarProduto (ProdutosDTO produto){
         
+        try {
+            conn = new conectaDAO().connectDB();
+            String sql="insert into produtos(nome,valor,status) values(?,?,?)";
+            prep=conn.prepareStatement(sql);
+            
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3,produto.getStatus());
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Cadastrado com sucesso!");
+        } catch (SQLException ex) {
+            
+        }
         
-        //conn = new conectaDAO().connectDB();
+        
         
         
     }
